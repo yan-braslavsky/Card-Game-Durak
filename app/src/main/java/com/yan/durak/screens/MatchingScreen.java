46b0c0b7c7370601edc39bfd62ecb4aa7c11583f
Yan Braslavskiy <iveliusster@gmail.com>
2015-08-29 11:28:03 +0200

    Matching screen recreated

--- app/src/main/java/com/yan/durak/screens/MatchingScreen.java
+++ app/src/main/java/com/yan/durak/screens/MatchingScreen.java
@@ -9,49 +9,64 @@ import glengine.yan.glengine.assets.atlas.YANAtlasTextureRegion;
 import glengine.yan.glengine.nodes.YANTexturedNode;
 import glengine.yan.glengine.renderer.YANGLRenderer;
 import glengine.yan.glengine.util.geometry.YANVector2;
+import glengine.yan.glengine.util.math.YANMathUtils;
 
 /**
  * Created by yan.braslavsky on 8/21/2015.
  */
 public class MatchingScreen extends BaseGameScreen {
 
-    private static final int AVATARS_COUNT = 10;
-    private static final int BASE_SORTING_LAYER = 10;
-    private final IGameServerConnector gameServerConnector;
-    private float angleSpeed = 40;
-    private float currentOffset = 0;
-    private ArrayList<YANTexturedNode> mAvatarList;
-    private YANVector2 mOriginalSize;
+    private static final int AVATARS_COUNT = 7;
+    private static float MOVEMENT_SPEED = 700;
+
+    private float mAnimationTimeBeforeNextScreen = 5;
+    private final PrototypeGameScreen mGameScreen;
+    private final ArrayList<YANTexturedNode> mAvatarList;
+    private final YANVector2 mOriginalSize;
+    private final YANAtlasTextureRegion[] mAvatarIcons;
+    private float mDistanceBetweenAvatars;
 
     public MatchingScreen(YANGLRenderer renderer, final IGameServerConnector gameServerConnector) {
         super(renderer);
-        this.gameServerConnector = gameServerConnector;
+        mGameScreen = new PrototypeGameScreen(renderer, gameServerConnector);
         mAvatarList = new ArrayList<>(AVATARS_COUNT);
+        mOriginalSize = new YANVector2();
+        mAvatarIcons = new YANAtlasTextureRegion[3];
     }
 
     @Override
     protected void onCreateNodes() {
         super.onCreateNodes();
+
+        //init all available icons
+        for (int i = 0; i < mAvatarIcons.length; i++) {
+            mAvatarIcons[i] = mUiAtlas.getTextureRegion("avatar_" + (i + 1) + ".png");
+        }
+
+        //create avatars
         for (int i = 0; i < AVATARS_COUNT; i++) {
-            int avatarNumber = (i % 3) + 1;
+            YANAtlasTextureRegion avatar = mAvatarIcons[(i % mAvatarIcons.length)];
             mAvatarList.add(NodeCreatorHelper.createAvatarBgWithTimerAndIcon(
-                    mUiAtlas.getTextureRegion("stump_bg.png"), mUiAtlas.getTextureRegion("avatar_" + avatarNumber + ".png"), true));
+                    mUiAtlas.getTextureRegion("stump_bg.png"), avatar, true));
         }
     }
 
     @Override
     protected void onChangeNodesSize() {
-        //TODO : Beware code repetitions !
+        calculateAvatarSize();
+        for (YANTexturedNode avatar : mAvatarList) {
+            avatar.setSize(mOriginalSize.getX(), mOriginalSize.getY());
+        }
+    }
+
+    private void calculateAvatarSize() {
+        //TODO : Beware code repetitions from node creator!
         final float bottomAvatarScaleFactor = 0.3f;
         YANAtlasTextureRegion textureRegion = mAvatarList.get(0).getTextureRegion();
         float aspectRatio = textureRegion.getWidth() / textureRegion.getHeight();
         float newWidth = getSceneSize().getX() * bottomAvatarScaleFactor;
         float newHeight = newWidth / aspectRatio;
-        mOriginalSize = new YANVector2(newWidth, newHeight);
-
-        for (YANTexturedNode avatar : mAvatarList) {
-            avatar.setSize(newWidth, newHeight);
-        }
+        mOriginalSize.setXY(newWidth, newHeight);
     }
 
     @Override
@@ -65,36 +80,18 @@ public class MatchingScreen extends BaseGameScreen {
     @Override
     protected void onLayoutNodes() {
         super.onLayoutNodes();
-        layoutInEllipse(0);
-    }
 
-    private void layoutInEllipse(final float angleOffset) {
-        float centerX = getSceneSize().getX() / 2;
-        float centerY = getSceneSize().getY() / 2;
-        float x;
-        float y;
-        float angle = angleOffset;
-        float angleStep = 360f / mAvatarList.size();
-        float radiusX = getSceneSize().getX() * 0.4f;
-        float radiusY = getSceneSize().getY() / 8;
-        float rad;
+        mDistanceBetweenAvatars = getSceneSize().getX() * 0.4f;
+        float screenHalfHeight = getSceneSize().getY() / 2f;
+
+        //set initial position
         for (int i = 0; i < mAvatarList.size(); i++) {
-            YANTexturedNode avatar = mAvatarList.get(i);
-            rad = (float) Math.toRadians(angle);
-            x = (float) ((double) centerX + (double) radiusX * Math.cos((double) rad));
-            y = (float) ((double) centerY + (double) radiusY * Math.sin((double) rad));
-            avatar.setPosition(x, y);
-
-            //sorting layer
-            avatar.setSortingLayer((angle > 180) ? BASE_SORTING_LAYER - Math.round(angle) : BASE_SORTING_LAYER + Math.round(angle));
-            angle += angleStep;
-            angle = angle % 360;
-
-            float percentage = Math.abs((angle / 360f) - 0.5f);
-            avatar.setSize(mOriginalSize.getX() * percentage, mOriginalSize.getY() * percentage);
-            avatar.setOpacity(percentage);
+            final YANTexturedNode avatar = mAvatarList.get(i);
+            avatar.setPosition((i * mDistanceBetweenAvatars) - mDistanceBetweenAvatars, screenHalfHeight);
+            avatar.setOpacity(0);
         }
     }
+
 
     @Override
     public void onSetActive() {
@@ -104,8 +101,38 @@ public class MatchingScreen extends BaseGameScreen {
     @Override
     public void onUpdate(float deltaTimeSeconds) {
         super.onUpdate(deltaTimeSeconds);
-        currentOffset += angleSpeed * deltaTimeSeconds;
-        currentOffset %= 360;
-        layoutInEllipse(currentOffset);
+        moveAvatars(MOVEMENT_SPEED * deltaTimeSeconds);
+
+        mAnimationTimeBeforeNextScreen -= deltaTimeSeconds;
+        if (mAnimationTimeBeforeNextScreen < 0)
+            getRenderer().setActiveScreen(mGameScreen);
+    }
+
+    private void moveAvatars(final float xDistance) {
+        YANTexturedNode offScreenAvatar = null;
+        final float offscreen = getSceneSize().getX() + mOriginalSize.getX();
+        float percentage = 0;
+        final float screenHalfWidth = getSceneSize().getX() / 2;
+        for (int i = 0; i < mAvatarList.size(); i++) {
+            final YANTexturedNode avatar = mAvatarList.get(i);
+            avatar.setPosition(avatar.getPosition().getX() + xDistance, avatar.getPosition().getY());
+            percentage = 1 - (Math.abs(avatar.getPosition().getX() - screenHalfWidth) / screenHalfWidth);
+            avatar.setOpacity(percentage);
+            avatar.setSize(mOriginalSize.getX() * percentage, mOriginalSize.getY() * percentage);
+            if (avatar.getPosition().getX() > offscreen) {
+                offScreenAvatar = avatar;
+            }
+        }
+
+        if (offScreenAvatar != null) {
+            final YANTexturedNode mostLeftAvatar = mAvatarList.get(mAvatarList.size() - 1);
+            mAvatarList.remove(offScreenAvatar);
+            offScreenAvatar.setPosition(mostLeftAvatar.getPosition().getX() - mDistanceBetweenAvatars,
+                    offScreenAvatar.getPosition().getY());
+            mAvatarList.add(offScreenAvatar);
+            YANTexturedNode icon = (YANTexturedNode) offScreenAvatar.getChildNodes().get(0).getChildNodes().get(0);
+            icon.setTextureRegion(mAvatarIcons[((int) Math.floor(YANMathUtils.randomInRange(0, mAvatarIcons.length)))]);
+        }
+
     }
 }
